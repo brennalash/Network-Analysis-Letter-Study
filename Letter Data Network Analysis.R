3f215d4a409b64e3c6dad5539560b52995d173be
Brenna L <brenna.lash@childrens.harvard.edu>
2021-02-27 18:13:51 -0600

    Update Letter Data Network Analysis.R

--- Letter Data Network Analysis.R
+++ Letter Data Network Analysis.R
@@ -47,126 +47,149 @@ Networkat <- estimateNetwork(letterdataa, default = "EBICglasso", corMethod = "c
 #creating network for anxiety symptoms- poly correlations
 
   #threshold = false
-na1 <- qgraph(cor_auto(letterdataa), graph = "glasso", sampleSize = nrow(letterdataa),  
-             theme = "colorblind", layout="spring", groups = Groupa, nodeNames = Namesa,
-             title = "EBICglasso Anxiety", cut = 0)
-   #warning: a dense regularized network was selected
+Network2a <- estimateNetwork(letterdataa, default = "EBICglasso", corMethod = "cor_auto") 
+#warning a dense regularized network was selected
 
   #threshold = true
-na2 <- qgraph(cor_auto(letterdataa), graph = "glasso", sampleSize = nrow(letterdataa), 
-             threshold = TRUE, theme = "colorblind", layout=na1$layout, 
-             nodeNames = Namesa, groups = Groupa, title = "Thresholded EBICglasso Anxiety", cut = 0)
-    #note: network with lowest lambda selected as best network
+Network2at <- estimateNetwork(letterdataa, default = "EBICglasso", corMethod = "cor_auto", threshold = TRUE)
+#note: network with lowest lambda was selected as best network
 
 #plotting the network for anxiety symptoms
 
   #threshold = false
-Networka_plot <- plot(Networka,layout = na1$layout, groups = Groupa, nodeNames = Namesa, title = "Spearman network Anxiety")
+Networka_plot <- plot(Networka,layout = "spring", groups = Groupa, nodeNames = Namesa, title = "Spearman network Anxiety")
 
   #threshold = true
-Networkat_plot <- plot(Networkat,layout = na1$layout, groups = Groupa, nodeNames = Namesa, title = "Thresholded spearman network Anxiety")
+Networkat_plot <- plot(Networkat,layout = Networka_plot$layout, groups = Groupa, nodeNames = Namesa, title = "Thresholded spearman network Anxiety")
 
-#------------------------------------------------------------------------------------------------------------------------------------------
+  #threshold = false
+Network2a_plot <- plot(Network2a,layout = Networka_plot$layout, groups = Groupa, nodeNames = Namesa, title = "Poly network Anxiety")
 
-#creating the network for depressive symptoms- spearman correlations
+  #threshold = true
+Network2at_plot <- plot(Network2at,layout = Networka_plot$layout, groups = Groupa, nodeNames = Namesa, title = "Thresholded Poly network Anxiety")
+
+#creating the network for depression symptoms- spearman correlations
 
   #threshold = false
 Networkd <- estimateNetwork(letterdatad, default = "EBICglasso", corMethod = "cor", corArgs = list(method = "spearman", use = "pairwise.complete.obs")) 
-    #warning: a dense regularized network was selected
+#warning a dense regularized network was selected
 
   #threshold = true
 Networkdt <- estimateNetwork(letterdatad, default = "EBICglasso", corMethod = "cor", corArgs = list(method = "spearman", use = "pairwise.complete.obs"), threshold = TRUE)
-    #note: network with lowest lambda was selected as best network
+#note: network with lowest lambda was selected as best network
 
 #creating network for depression symptoms- poly correlations
 
   #threshold = false
-nd1 <- qgraph(cor_auto(letterdatad), graph = "glasso", sampleSize = nrow(letterdatad),  
-              theme = "colorblind", layout="spring", nodeNames = Namesd, groups = Groupd, title = "EBICglasso Depression", cut = 0)
-    #no warning message
+Network2d <- estimateNetwork(letterdatad, default = "EBICglasso", corMethod = "cor_auto") 
+#warning a dense regularized network was selected
 
   #threshold = true
-nd2 <- qgraph(cor_auto(letterdatad), graph = "glasso", sampleSize = nrow(letterdatad), threshold = TRUE,
-              theme = "colorblind", layout= nd1$layout, nodeNames = Namesd, groups = Groupd, title = "Threshold EBICglasso Depression", cut = 0)
-    #no warning message
+Network2dt <- estimateNetwork(letterdatad, default = "EBICglasso", corMethod = "cor_auto", threshold = TRUE)
+#note: network with lowest lambda was selected as best network
 
 #plotting the network for depression symptoms
+
   #threshold = false
-Networka_plot <- plot(Networkd,layout = nd1$layout, groups = Groupd, nodeNames = Namesd, title = "Spearman network Depression")
+Networkd_plot <- plot(Networkd,layout = "spring", groups = Groupd, nodeNames = Namesd, title = "Spearman network Depression")
+
   #threshold = true
-Networkat_plot <- plot(Networkdt,layout = nd1$layout, groups = Groupd, nodeNames = Namesd, title = "Thresholded spearman network Depression")
+Networkdt_plot <- plot(Networkdt,layout = Networkd_plot$layout, groups = Groupd, nodeNames = Namesd, title = "Thresholded spearman network Depression")
+
+  #threshold = false
+Network2d_plot <- plot(Network2d,layout = Networkd_plot$layout, groups = Groupd, nodeNames = Namesd, title = "Poly network Depression")
+
+  #threshold = true
+Network2dt_plot <- plot(Network2dt,layout = Networkd_plot$layout, groups = Groupd, nodeNames = Namesd, title = "Thresholded Poly network Depression")
 
 #--------------------------------------centrality inferences---------------------------------------------------------------------------------
 
 #centrality plot for network for anxiety symptoms - 
 centralityPlot(Networka, include = c("ExpectedInfluence", "Strength"), orderBy = "ExpectedInfluence")
-centralityPlot(na1, include = c("ExpectedInfluence", "Strength"), orderBy = "ExpectedInfluence")
+centralityPlot(Network2a, include = c("ExpectedInfluence", "Strength"), orderBy = "ExpectedInfluence")
 
 #centrality plot for network for depressive symptoms
 centralityPlot(Networkd, include = c("ExpectedInfluence", "Strength"), orderBy = "ExpectedInfluence")
-centralityPlot (nd1, include = c("ExpectedInfluence", "Strength"), orderBy = "ExpectedInfluence") 
+centralityPlot (Network2d, include = c("ExpectedInfluence", "Strength"), orderBy = "ExpectedInfluence") 
 
 #numerical centrality values
 centrality(Networka)$InExpectedInfluence
-centrality(na1)$InExpectedInfluence
+centrality(Network2a)$InExpectedInfluence
 centrality(Networkd)$InExpectedInfluence
-centrality(nd1)$InExpectedInfluence
+centrality(Network2d)$InExpectedInfluence
 
 #--------------------------------------network stability---------------------------------------------------------------------------------
 
 #adjacency matrix
 Networka$graph
-na1$graph
+Network2a$graph
 Networkd$graph
-nd1$graph
+Network2d$graph
 
 #bootstrap routines, non-parametric bootstrap when handling ordinal data
 
     #anxiety network
 boot_spearmana <- bootnet(Networka, nCores=8, nBoots=2500, statistics = c("edge", "strength", "expectedInfluence"))
-boot_polya <- bootnet(na1, nCores=8, nBoots=2500, statistics = c("edge", "strength", "expectedInfluence"))
+boot_polya <- bootnet(Network2a, nCores=8, nBoots=2500, statistics = c("edge", "strength", "expectedInfluence"))
 
 boot2_spearmana <- bootnet(Networka, nCores=8, nBoots=2500, type="case", statistics = c("edge", "strength", "expectedInfluence"))
-boot2_polya <- bootnet(na1, nCores=8, type = "case", nBoots=2500, statistics = c("edge", "strength", "expectedInfluence"))
+boot2_polya <- bootnet(Network2a, nCores=8, type = "case", nBoots=2500, statistics = c("edge", "strength", "expectedInfluence"))
 
     #depression network
-boot_spearmand <- bootnet(Networkd, nCores=8, nBoots=2500, type="case", statistics = c("edge", "strength", "expectedInfluence"))
-boot_polyd <- bootnet(nd1, nCores=8, nBoots=2000, type="case", statistics = c("edge", "strength", "expectedInfluence"))
+boot_spearmand <- bootnet(Networkd, nCores=8, nBoots=2500, statistics = c("edge", "strength", "expectedInfluence"))
+boot_polyd <- bootnet(Network2d, nCores=8, nBoots=2500, statistics = c("edge", "strength", "expectedInfluence"))
 
 boot2_spearmand <- bootnet(Networkd, nCores=8, nBoots=2500, type="case", statistics = c("edge", "strength", "expectedInfluence"))
-boo2t_polyd <- bootnet(nd1, nCores=8, nBoots=2000, type="case", statistics = c("edge", "strength", "expectedInfluence"))
+boot2_polyd <- bootnet(Network2d, nCores=8, nBoots=2000, type="case", statistics = c("edge", "strength", "expectedInfluence"))
 
 #plot edge weights CI
   
   #anxiety network
 plot(boot_spearmana, labels = FALSE, order = "sample")
 plot(boot_polya, labels = FALSE, order = "sample") 
-  #
+
+  #depression network
 plot(boot_spearmand, labels = FALSE, order = "sample") 
 plot(boot_polyd, labels = FALSE, order = "sample") 
 
 
 #diff test edge weights
-plot(b1EtreshSP, "edge", plot = "difference", onlyNonZero = TRUE, order = "sample")
-plot(b1EtreshSP, "edge", plot = "difference", onlyNonZero = TRUE, order = "sample")
-plot(b1EtreshSP, "edge", plot = "difference", onlyNonZero = TRUE, order = "sample")
-plot(b1EtreshSP, "edge", plot = "difference", onlyNonZero = TRUE, order = "sample")
+
+  #anxiety network
+plot(boot_spearmana, "edge", plot = "difference", onlyNonZero = TRUE, order = "sample")
+plot(boot_polya, "edge", plot = "difference", onlyNonZero = TRUE, order = "sample")
+
+  #depression network
+plot(boot_spearmand, "edge", plot = "difference", onlyNonZero = TRUE, order = "sample")
+plot(boot_polyd, "edge", plot = "difference", onlyNonZero = TRUE, order = "sample")
 
 #expected influence plot stability
-plot(b2EtreshSP, statistics = "expectedInfluence")
-plot(b2EtreshSP, statistics = "expectedInfluence")
-plot(b2EtreshSP, statistics = "expectedInfluence")
-plot(b2EtreshSP, statistics = "expectedInfluence")
+
+  #anxiety network
+plot(boot2_spearmana, statistics = "expectedInfluence")
+plot(boot2_polya, statistics = "expectedInfluence")
+
+  #depression network
+plot(boot2_spearmand, statistics = "expectedInfluence")
+plot(boot2_polyd, statistics = "expectedInfluence")
 
 #stability coefficients
-corStability(b2EtreshSP, statistics = "expectedInfluence")
-corStability(b2EtreshSP, statistics = "expectedInfluence")
-corStability(b2EtreshSP, statistics = "expectedInfluence")
-corStability(b2EtreshSP, statistics = "expectedInfluence")
+
+  #anxiety network
+corStability(boot2_spearmana, statistics = "expectedInfluence")
+corStability(boot2_polya, statistics = "expectedInfluence")
+
+  #depression network
+corStability(boot2_spearmand, statistics = "expectedInfluence")
+corStability(boot2_polyd, statistics = "expectedInfluence")
 
 #diff test expected influence
-plot(b1EtreshSP, statistics = "expectedInfluence", order="sample", labels=TRUE)
-plot(b1EtreshSP, statistics = "expectedInfluence", order="sample", labels=TRUE)
-plot(b1EtreshSP, statistics = "expectedInfluence", order="sample", labels=TRUE)
-plot(b1EtreshSP, statistics = "expectedInfluence", order="sample", labels=TRUE)
+
+  #anxiety network
+plot(boot_spearmana, statistics = "expectedInfluence", order="sample", labels=TRUE)
+plot(boot_polya, statistics = "expectedInfluence", order="sample", labels=TRUE)
+
+  #depression network
+plot(boot_spearmand, statistics = "expectedInfluence", order="sample", labels=TRUE)
+plot(boot_polyd, statistics = "expectedInfluence", order="sample", labels=TRUE)
 
